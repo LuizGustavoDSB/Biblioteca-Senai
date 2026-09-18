772d07d42550b0bb2fef00ea158857f260f3a250
Luiz Gustavo de Souza Bittencourt <ziul666x@gmail.com>
2026-09-17 23:29:51 -0300

    Livro.java
    Terminado.

--- src/Livro.java
+++ src/Livro.java
@@ -1,12 +1,59 @@
 public class Livro {
 
-    String tituloLivro;
-    String codigoDoLivro;
+    private String tituloLivro;
+    private String codigoDoLivro;
+    private boolean alugado;
+    private Usuario usuarioAluguel;
+
+    public Livro(String tituloLivro) {
+
+        this.tituloLivro = tituloLivro;
+        this.alugado = false;
+        this.usuarioAluguel = null;
+
+    }
+
+    public String getTituloLivro() {
+        return tituloLivro;
+    }
+
+    public void setTituloLivro(String tituloLivro) {
+
+        this.tituloLivro = tituloLivro;
+    }
+
+    public String getCodigoDoLivro() {
+        return codigoDoLivro;
+    }
+
+    public void setCodigoDoLivro(String codigoDoLivro) {
+
+        this.codigoDoLivro = codigoDoLivro;
+    }
+
+    public boolean isAlugado() {
+        return alugado;
+    }
+
+    public Usuario getUsuarioAluguel() {
+
+        return usuarioAluguel;
+    }
+
+    public void alugar(Usuario usuario) {
+
+        this.alugado = true;
+        this.usuarioAluguel = usuario;
+    }
+
+    public void devolver() {
+        this.alugado = false;
+        this.usuarioAluguel = null;
+    }
 
     @Override
     public String toString() {
-        return "Livro '" + tituloLivro + "'" +
-                ", código do livro é: '" + codigoDoLivro + "'";
+        return "Livro " + tituloLivro + ", código do livro é: '" + codigoDoLivro + "'";
+
     }
 }
-
